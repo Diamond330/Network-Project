6e9990ed3404829e09bc564472488485d4b198a2
sookho <howardsookho@gmail.com>
2022-05-10 20:48:55 -0400

    fixed

--- attacker.py
+++ attacker.py
@@ -12,70 +12,48 @@ class Attacker(Client):
                    request: List[Tuple], 
                    server: Server
     ) -> int:
-        """
-        record a victim user's history request, try to 
-        recover the AES key of that session of request.
-        """
-        # C: the encryted AES key that we want to hack
         victim_id, C, _ = request[0]
         n, e = public_key
 
         k_b = 0
         for b in range(127, -1, -1):
-            # assume that the encode way is naive encoding
-            # according to the fundamental property of multiplication
-            # in modular arithmetic, we should compute C_b for each
-            # block of this data.
             C_b = [c * ((1 << b*e) % n) % n for c in C]
-
-            # after leftshift, the aes key will be longer than 128 bit.
-            # recall that the server only use the least 128bit as the AES key.
-            # we mask the least 128bit here.
             aes_key = utils.bit_mask(128) & (k_b << b)
             aes = AES.new(aes_key.to_bytes(16, 'big'))
-
-            # send a dummy WUP request to obtain the server's attitude
-            # towards the current hacked aes key.
-            wups = WUP("Hack processing", self.mac, self.imei)
+            wups = WUP_request("Hack processing", self.mac, self.imei)
             encrypted_wups = [aes.encrypt(wup) for wup in wups]
-
             req = [(victim_id, C_b, wup) for wup in encrypted_wups]
-
-            # update the hacked aes key according to server's respondence.
             k_b = (1-server.process_request(req)) << (127-b) | k_b
 
-        # use the hacked AES key to encrypt another plain text
         victim_aes = k_b
         print("hacked AES key: {}".format(victim_aes))
 
         aes = AES.new(victim_aes.to_bytes(16, 'big'))
 
-        wups = WUP("Hacking successfully", self.mac, self.imei)
+        wups = WUP_request("Hacking successfully", self.mac, self.imei)
         encrypted_wups = [aes.encrypt(wup) for wup in wups]
 
         req = [(victim_id, C, wup) for wup in encrypted_wups]
         assert server.process_request(req) == True, "Hack failed"
 
-        # further decrypt the victim request
-        victim_text = ""
+        original_text = ""
         for _, _, req in request:
-            dt = aes.decrypt(req)
-            text, checksum = dt[:-64], dt[-64:]
-
+            decrypted_text = aes.decrypt(req)
+            text, checksum = decrypted_text[:-64], decrypted_text[-64:]
             content, mac, imei = text.decode('utf-8').split('\t')
-            victim_text += content.strip()
-        print("Decrypted Cyphertext: {}".format(victim_text))
+            original_text += content.strip()
+        print("Decrypted Cyphertext: {}".format(original_text))
 
         return victim_aes
 
 
 def test_hack(encode_method):
-    user1 = Client(rsa_encode_method = encode_method)
-    req1 = user1.send_request("UVA is an iconic public institution of higher education.")
+    user = Client(rsa_encode_method = encode_method)
+    req = user.send_request("UVA is an iconic public institution of higher education.")
 
     server = Server()
-    server.register(user1)
+    server.register(user)
 
     attacker = Attacker()
-    attacker.hack(user1.rsa.public_key, req1, server)
+    attacker.hack(user.rsa.public_key, req, server)
 
--- main.py
+++ main.py
@@ -3,18 +3,16 @@ from communication_model import test_communicate
 from attacker import test_hack
 
 def main():
-    print("="*20, "Test for Part 1", "="*20)
+    print("*"*20, "Test for RSA", "*"*20)
     test_rsa("naive")
 
-    print("="*20, "Test for Part 2", "="*20)
+    print("*"*20, "Test for CCA2 attack", "*"*20)
     test_communicate()
     test_hack("naive")
 
-    print("="*20, "Test for Part 3", "="*20)
+    print("*"*20, "Test for OAEP", "*"*20)
     test_rsa("oaep")
     test_oaep_encode_and_decode()
-
-    # bonus attack on oaep-rsa
     test_hack("oaep")
 
 if __name__ == "__main__":
